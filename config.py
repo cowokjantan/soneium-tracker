dd8cf86b1308f41203ac50b8964c6aa424dd5ffa
cowok jantan <85976240+cowokjantan@users.noreply.github.com>
2025-02-14 07:49:09 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")  # Token Telegram bot
+BLOCKSCOUT_API = "https://soneium.blockscout.com/api"  # API Blockscout
+DATABASE_URL = os.getenv("DATABASE_URL")  # Database untuk menyimpan data
